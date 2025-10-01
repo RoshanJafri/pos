f377339196393a0846e4a4f17f2dd62282e18f18
Roshan Jafri <roshanjafri31@gmail.com>
2025-10-01 16:37:32 +0200

    Added password protection to Sales

--- resources/views/orders/accounts.blade.php
+++ resources/views/orders/accounts.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 
 @push('content')
+
+
+  <div id="password-overlay">
+    <h2>Enter Password</h2>
+    <input type="password" id="page-password" placeholder="Password">
+    <button id="submit-password">Unlock</button>
+	
+        <a href="{{url('')}}">Go back</a>
+    <div id="wrong-pass">Wrong password</div>
+  </div>
     <div class="container">
         <div class="row">
             <div class="col-lg-8">
@@ -89,7 +99,7 @@
             {{-- Sales Summary --}}
             <div class="col-lg-4">
                 @if (strlen($start_date) < 1)
-                    <h4>Total Sales {{ $period ? ucfirst(str_replace('_', ' ', $period)) : 'Today' }}</h4>
+                    <h4>Total Sales {{ $period ? ucfirst(str_replace('_', ' ', $period)) : '' }}</h4>
                     Today's date: {{ date('jS \o\f M, Y') }}
                     <h1>Rs. {{ $total }}</h1>
                 @else
@@ -101,4 +111,39 @@
             </div>
         </div>
     </div>
+    
+@endpush
+@push('scripts')
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        document.querySelectorAll('.btn-delete').forEach(function (button) {
+            button.addEventListener('click', function () {
+                var action = this.getAttribute('data-action');
+                document.getElementById('deleteForm').setAttribute('action', action);
+            });
+        });
+    });
+</script>
+    <script src="{{ asset('') }}assets/libs/bootstrap/js/bootstrap-bundle.min.js"></script>
+      <script>
+    const correctPassword = "3112"; // 🔐 Change this to your desired password
+
+    $('#submit-password').click(function () {
+
+      const input = $('#page-password').val();
+
+      if (input === correctPassword) {
+        $('#password-overlay').fadeOut();
+        $('body').removeClass('locked');
+      } else {
+        $('#wrong-pass').fadeIn();
+      }
+    });
+
+    $('#page-password').on('keypress', function (e) {
+      if (e.which === 13) {
+        $('#submit-password').click();
+      }
+    });
+  </script>
 @endpush
